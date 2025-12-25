02a896755160bebc7ccf7e0996f446c9dba06a59
kurushiba <156669290+kurushiba@users.noreply.github.com>
2025-12-25 12:50:30 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ja'>
+    <html lang="ja">
       <body>
+        {/* <header
+          style={{ backgroundColor: '#333', color: '#fff', padding: '15px 0' }}
+        >
+          <div
+            className="container"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <h1>
+              <span style={{ fontSize: '24px', fontWeight: 'bold' }}>
+                BBS App
+              </span>
+            </h1>
+          </div>
+        </header> */}
         <Header />
-        <main className='container' style={{ padding: '40px 20px' }}>
+        <main className="container" style={{ padding: '40px 20px' }}>
           {children}
         </main>
       </body>
